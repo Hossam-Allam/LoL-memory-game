dda811ff22fada3c175a8ed5b0ea312334bec57f
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-07-02 04:36:25 +0300

    initial game and card components

---
+++ src/components/card.jsx
@@ -0,0 +1,8 @@
+function Card() {
+  return (
+    <div className="card">
+      <h1>Hi Im a card</h1>
+    </div>
+  );
+}
+export default Card;
---
+++ src/components/game.jsx
@@ -0,0 +1,11 @@
+import Card from "./card";
+function Game() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default Game;
