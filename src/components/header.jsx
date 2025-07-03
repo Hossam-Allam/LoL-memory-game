757bc36e013de9a06160ead9addd1d48ff427450
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-07-03 22:49:48 +0300

    header layout update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
 function Header({ streak, attempts }) {
   return (
     <div className="header">
-      <h1>Memory Game</h1>
-      <p>Current Streak: {streak}</p>
-      <p>Attempts: {attempts}</p>
+      <h1 className="title">Memory Game</h1>
+      <div className="stats">
+        <p>Current Streak: {streak}</p>
+        <p>Attempts: {attempts}</p>
+      </div>
     </div>
   );
 }
