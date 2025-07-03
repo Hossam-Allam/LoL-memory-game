e27a93aa550a45ffa3f9ae4bf8540673c886718d
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-07-03 23:05:09 +0300

    added tilt effect

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+
 function Card({ name, imageUrl, onClick }) {
   return (
-    <div className="card" onClick={onClick}>
-      <div className="card-img-container">
-        <img src={imageUrl} alt={name} className="card-img" />
+    <Tilt
+      className="card-tilt-wrapper"
+      tiltMaxAngleX={12}
+      tiltMaxAngleY={12}
+      perspective={600}
+      scale={1.03}
+      transitionSpeed={400}
+      glareEnable={false}
+    >
+      <div className="card" onClick={onClick}>
+        <div className="card-img-container">
+          <img src={imageUrl} alt={name} className="card-img" />
+        </div>
+        <div className="card-name">{name}</div>
       </div>
-      <div className="card-name">{name}</div>
-    </div>
+    </Tilt>
   );
 }
 export default Card;
