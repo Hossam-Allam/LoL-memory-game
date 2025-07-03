d5abc6d8fb370075588ca29a9e0dbc1c42fc0a33
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-07-03 23:26:40 +0300

    added league of legends logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,14 @@
 function Header({ streak, attempts }) {
   return (
     <div className="header">
-      <h1 className="title">LoL Memory Game</h1>
+      <div className="header-left">
+        <img
+          src="https://i.pinimg.com/474x/2d/cc/36/2dcc363a73f27511e51969ac8cc15b92.jpg"
+          alt="League of Legends classic"
+          className="lol"
+        />
+        <h1 className="title">Memory Game</h1>
+      </div>
       <div className="stats">
         <p>Current Streak: {streak}</p>
         <p>Attempts: {attempts}</p>
